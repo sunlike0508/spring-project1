508ac60657e3739222e6548abca658bc7bed5cc5
seonhoshin <sunlike0508@waug.com>
2024-08-29 15:04:11 +0900

    초기화

---
+++ src/main/java/hello/springmvc/SpringProject1Application.java
@@ -0,0 +1,13 @@
+package hello.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProject1Application.class, args);
+    }
+
+}
---
+++ src/test/java/hello/springmvc/SpringProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
